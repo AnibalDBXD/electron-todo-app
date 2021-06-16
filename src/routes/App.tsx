fce02e104be48327f1687d3724f8d38abb822c69
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-16 14:20:11 -0400

    Fix suspense error

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,22 +1,26 @@
+import { Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import ROUTES from "./routes";
+import Loading from "../components/Loading";
 
 const AppRouter = (): JSX.Element => (
-  <Router>
-    <Switch>
-      {ROUTES.map(({
-        component, exact, name, path,
-      }) => (
-        <Route
-          component={component}
-          exact={exact && exact}
-          key={name}
-          path={path}
-        />
-      ))}
-    </Switch>
-  </Router>
+  <Suspense fallback={<Loading />}>
+    <Router>
+      <Switch>
+        {ROUTES.map(({
+          component, exact, name, path,
+        }) => (
+          <Route
+            component={component}
+            exact={exact && exact}
+            key={name}
+            path={path}
+          />
+        ))}
+      </Switch>
+    </Router>
+  </Suspense>
 );
 
 export default AppRouter;
