bd3c32906cc3e22df2919300ab0d0786651e0578
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-20 18:08:16 -0400

    Update Task on check save value

--- src/pages/Home/List/Task/index.tsx
+++ src/pages/Home/List/Task/index.tsx
@@ -1,5 +1,5 @@
 import {
-  Text, Checkbox, Grid, IconButton, GridItem,
+  Text, Checkbox, Grid, IconButton, GridItem, useBoolean,
 } from "@chakra-ui/react";
 import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
 import { useHistory } from "react-router-dom";
@@ -13,8 +13,16 @@ dayjs.extend(relativeTime);
 const Task = ({
   color, name, time, id, isChecked,
 }: ITask) => {
-  const { deleteTask } = useTaskContext();
+  const { deleteTask, editTask } = useTaskContext();
   const { push } = useHistory();
+  const [currentIsChecked, setChecked] = useBoolean(isChecked);
+
+  const handleCheck = () => {
+    setChecked.toggle();
+    editTask?.({
+      color, id, name, time, isChecked: currentIsChecked,
+    });
+  };
   return (
     <Grid gridGap={2} templateColumns="repeat(3, 1fr)" templateRows="repeat(3, 1fr)" padding={4} backgroundColor={color} borderRadius="20%" w="215px" h="215px">
       <GridItem paddingRight={2} alignSelf="flex-start" justifySelf="flex-end" colSpan={3}>
@@ -24,7 +32,7 @@ const Task = ({
         <Text color="white" textOverflow="clip" fontSize={20}>{name}</Text>
       </GridItem>
       <GridItem alignSelf="center" justifySelf="center">
-        <Checkbox isChecked={isChecked} size="lg" />
+        <Checkbox onClick={handleCheck} isChecked={currentIsChecked} size="lg" />
       </GridItem>
       <GridItem
         colSpan={2}
