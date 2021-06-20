f76eed43122b3366e03dade2d39eb31704d66f98
AnibalDBXD <olivaresluisanibal@gmail.com>
2021-06-20 17:56:13 -0400

    Fix npm run build

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import ROUTES from "./routes";
 import Loading from "../components/Loading";
